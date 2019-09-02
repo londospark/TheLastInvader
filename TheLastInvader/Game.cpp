145beb29b30d8371157ea870798b4687a7a7e3a2
Gareth Hubball <ridecar2@gmail.com>
2019-09-03 00:20:15 +0100

    Desctruction lines on a block are now cleared whenever we demolish that block.

--- TheLastInvader/Game.cpp
+++ TheLastInvader/Game.cpp
@@ -4,23 +4,34 @@
 
 //TODO(gareth): Is building really an object? Should blocks be objects, that way they can contain the destruction lines in them.
 
-enum player_direction {
-	LEFT, RIGHT, DOWN
+const int starting_durability = 10;
+
+struct Line {
+	float x1, y1, x2, y2;
 };
 
-struct player_movement {
-	player_direction direction;
+struct Building {
+	int height;
+	int durability = starting_durability;
+	std::vector<Line> desctruction_lines;
+
+	Building(int height) : height(height)
+	{}
+};
+
+struct PlayerMovement {
+	enum Direction {
+		LEFT, RIGHT, DOWN
+	};
+	
+	Direction direction;
 	olc::vf2d position;
 };
 
-struct bullet {
+struct Bullet {
 	olc::vf2d position;
 	bool collided;
 	bool beneath_screen;
-};
-
-struct line {
-	float x1, y1, x2, y2;
 };
 
 float normalised_random() {
@@ -72,13 +83,13 @@ public:
 		//DrawRect(player_movement.position.x, player_movement.position.y, 10, 10);
 		DrawSprite(player_movement.position.x, player_movement.position.y, player_sprite, scale);
 
-		float building_width = ScreenWidth() / building_heights.size();
+		float building_width = ScreenWidth() / buildings.size();
 		std::vector<bool> building_collisions{ false, false, false, false, false, false, false, false, false, false };
 
 		// Collision of bullets and buildings
 		for (auto& bullet : bullets) {
 			auto index = std::floor(bullet.position.x / building_width);
-			auto building_height = building_heights[index] * block_height;
+			auto building_height = buildings[index].height * block_height;
 
 			if (building_height == 0 && bullet.position.y > ScreenHeight()) {
 				bullet.beneath_screen = true;
@@ -98,15 +109,16 @@ public:
 		{
 			if (building_collisions[i])
 			{
-				building_durability[i]--;
-				if (building_durability[i] == 0) {
-					building_heights[i]--;
-					building_durability[i] = starting_durability;
+				buildings[i].durability--;
+				if (buildings[i].durability == 0) {
+					buildings[i].height--;
+					buildings[i].durability = starting_durability;
+					buildings[i].desctruction_lines = {};
 				}
 				else {
 					float left = i * building_width;
 					float right = (i + 1) * building_width;
-					float top = ScreenHeight() - (building_heights[i] * block_height);
+					float top = ScreenHeight() - (buildings[i].height * block_height);
 					float bottom = top + block_height;
 
 					float x1 = left + (right - left) * normalised_random();
@@ -114,7 +126,7 @@ public:
 
 					float y1 = top + (bottom - top) * normalised_random();
 					float y2 = top + (bottom - top) * normalised_random();
-					desctruction_lines.push_back({ x1, y1, x2, y2 });
+					buildings[i].desctruction_lines.push_back({ x1, y1, x2, y2 });
 				}
 			}
 		}
@@ -122,17 +134,18 @@ public:
 		// Draw the buildings to destroy
 		auto left = 0;
 
-		for (auto height : building_heights) {
-			FillRect(left, ScreenHeight() - height * block_height, building_width, height * block_height, olc::GREEN);
+		for (auto building : buildings) {
+			FillRect(left, ScreenHeight() - building.height * block_height, building_width, building.height * block_height, olc::GREEN);
+
+			for (auto line : building.desctruction_lines) {
+				DrawLine(line.x1, line.y1, line.x2, line.y2, olc::BLACK);
+			}
+
 			left += building_width;
 		}
 
-		for (auto line : desctruction_lines) {
-			DrawLine(line.x1, line.y1, line.x2, line.y2, olc::BLACK);
-		}
-
 		// Filter out old bullets
-		bullets.remove_if([](const bullet& bullet) { return bullet.collided || bullet.beneath_screen; });
+		bullets.remove_if([](const Bullet& bullet) { return bullet.collided || bullet.beneath_screen; });
 
 		// Draw the bullets
 		for (auto& bullet : bullets) {
@@ -143,48 +156,49 @@ public:
 		return true;
 	}
 
-	player_movement MovePlayer(player_movement movement, float displacement)
+	PlayerMovement MovePlayer(PlayerMovement movement, float displacement)
 	{
-		if (movement.direction == RIGHT)
+		using Direction = PlayerMovement::Direction;
+		if (movement.direction == Direction::RIGHT)
 		{
 			float right_boundary = ScreenWidth() - margin - player_width;
 			float newPos{ movement.position.x + displacement };
 			if (newPos <= right_boundary) {
-				return { RIGHT, {newPos, movement.position.y} };
+				return { Direction::RIGHT, {newPos, movement.position.y} };
 			}
 			else {
-				return { DOWN, {right_boundary, movement.position.y + (newPos - right_boundary) } };
+				return { Direction::DOWN, {right_boundary, movement.position.y + (newPos - right_boundary) } };
 			}
 		}
 
-		if (movement.direction == DOWN)
+		if (movement.direction == Direction::DOWN)
 		{
 			auto current_layer = std::floor(movement.position.y / layer_height);
 			auto new_layer = std::floor((movement.position.y + displacement) / layer_height);
 			
 			if (current_layer == new_layer) {
-				return { DOWN, {movement.position.x, movement.position.y + displacement} };
+				return { Direction::DOWN, {movement.position.x, movement.position.y + displacement} };
 			}
 			else {
 				auto y = new_layer * layer_height;
 				auto x_movement = movement.position.y + displacement - y;
 				if (movement.position.x <= margin) {
-					return { RIGHT, { movement.position.x + x_movement, y } };
+					return { Direction::RIGHT, { movement.position.x + x_movement, y } };
 				}
 				else {
-					return { LEFT, { movement.position.x - x_movement, y } };
+					return { Direction::LEFT, { movement.position.x - x_movement, y } };
 				}
 			}
 		}
 
-		if (movement.direction == LEFT)
+		if (movement.direction == Direction::LEFT)
 		{
 			auto newPos = movement.position.x - displacement;
 			if (newPos >= margin) {
-				return { LEFT, {newPos, movement.position.y} };
+				return { Direction::LEFT, {newPos, movement.position.y} };
 			}
 			else {
-				return { DOWN, {margin, movement.position.y + (margin - newPos) } };
+				return { Direction::DOWN, {margin, movement.position.y + (margin - newPos) } };
 			}
 		}
 
@@ -201,30 +215,15 @@ private:
 	const float margin = 20;
 	const int layer_height = 20;
 
-	const int starting_durability = 10;
-
 	const int player_width = base_width * scale;
 	const int player_height = base_height * scale;
 
 	const int block_height = 15;
 
-	std::vector<int> building_durability{
-		starting_durability,
-		starting_durability,
-		starting_durability,
-		starting_durability,
-		starting_durability,
-		starting_durability,
-		starting_durability,
-		starting_durability,
-		starting_durability,
-		starting_durability };
-	std::vector<int> building_heights{ 4, 5, 3, 2, 6, 4, 6, 3, 5, 2 };
-	
-	std::vector<line> desctruction_lines;
+	std::vector<Building> buildings{ 4, 5, 3, 2, 6, 4, 6, 3, 5, 2 };
 
-	std::list<bullet> bullets;
-	player_movement player_movement{ RIGHT, { 20.0f, 20.0f } };
+	std::list<Bullet> bullets;
+	PlayerMovement player_movement{ PlayerMovement::Direction::RIGHT, { 20.0f, 20.0f } };
 
 	olc::Sprite* player_sprite;
 };
